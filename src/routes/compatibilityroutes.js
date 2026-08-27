288c36fbc75c18420c2e4186894f608aeb44f077
juanMorales02 <sebasmorales.lisurp@gmail.com>
2026-08-26 19:13:16 -0500

    Delete src/routes/compatibilityroutes.js

--- src/routes/compatibilityroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verifyToken = require('../middlewares/authMiddleware');
-const { checkCompatibility, getHistory } = require('../controllers/compatibilityController');
-
-router.post('/check', verifyToken, checkCompatibility);
-router.get('/history', verifyToken, getHistory);
-
-module.exports = router;
