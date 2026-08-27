63aa138b38d4632a36a2294c45a43b59ad588ce3
juanMorales02 <sebasmorales.lisurp@gmail.com>
2026-08-26 19:12:25 -0500

    Delete src/routes/numerologyroutes.js

--- src/routes/numerologyroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verifyToken = require('../middlewares/authMiddleware');
-const { calculate, getProfile } = require('../controllers/numerologyController');
-
-router.post('/calculate', verifyToken, calculate);
-router.get('/profile', verifyToken, getProfile);
-
-module.exports = router;
